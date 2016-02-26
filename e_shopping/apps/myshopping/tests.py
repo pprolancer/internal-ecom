c00cf328858d4d7419cb0f8aaf29b2edbaecd610
cis-yogesh <yogesh.p@cisinlabs.com>
2016-02-26 22:35:35 +0530

    First  test case for Category,product

--- e_shopping/apps/myshopping/tests.py
+++ e_shopping/apps/myshopping/tests.py
@@ -1,3 +1,60 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.contrib.auth.models import User
+from myshopping.models import Category, Product
+
+
+class CategoryTestCase(TestCase):
+    fixtures = ["tests.json"]
+
+    def setUp(self):
+        super(CategoryTestCase, self).setUp()
+        self.category_1 = Category.objects.get(pk=1)
+        self.category_2 = Category.objects.get(pk=2)
+        self.category_3 = Category.objects.get(pk=3)
+
+    def test_pass_stringification(self):
+        """
+        Test the stringification of a ``User`` object. A
+        "human-readable" representation of an ``User`` object.
+        """
+        category_name_1 = self.category_1.title
+        self.assertEqual(self.category_1.__str__(), category_name_1, "USER STRING METHOD WORK")
+
+    def test_fail_stringification(self):
+        """
+        Test the stringification of a ``User`` object. A
+        "human-readable" representation of an ``User`` object.
+        """
+
+        category_name_2 = self.category_2.title
+        self.assertEqual(self.category_name_2.__str__(), category_name_2, "USER STRING METHOD NOT WORK")
+
+
+class ProductTestCase(TestCase):
+    fixtures = ["tests.json"]
+
+    def setUp(self):
+        super(ProductTestCase, self).setUp()
+        # self.product_1 = Product.objects.get(pk=1)
+        # import pdb;pdb.set_trace()
+        self.product_2 = Product.objects.get(pk=2)
+        self.product_3 = Product.objects.get(pk=3)
+
+    def test_pass_stringification(self):
+        """
+        Test the stringification of a ``Product`` object. A
+        "human-readable" representation of an ``Product`` object.
+        """
+        product_name_2 = self.product_2.product_name
+        self.assertEqual(self.product_2.__str__(), product_name_2, "USER STRING METHOD WORK")
+
+    def test_fail_stringification(self):
+        """
+        Test the stringification of a ``Product`` object. A
+        "human-readable" representation of an ``Product`` object.
+        """
+
+        product_name_3 = self.product_3.product_name
+        self.assertEqual(self.product_3.__str__(), product_name_3, "USER STRING METHOD NOT WORK")        
