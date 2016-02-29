4daa599afbc88ae7a8e4ba8a7f5946dfa0b4331c
cis-yogesh <yogesh.p@cisinlabs.com>
2016-02-29 14:31:12 +0530

    test cases first commit

--- e_shopping/apps/myshopping/tests.py
+++ e_shopping/apps/myshopping/tests.py
@@ -12,7 +12,6 @@ class CategoryTestCase(TestCase):
         super(CategoryTestCase, self).setUp()
         self.category_1 = Category.objects.get(pk=1)
         self.category_2 = Category.objects.get(pk=2)
-        self.category_3 = Category.objects.get(pk=3)
 
     def test_pass_stringification(self):
         """
@@ -29,7 +28,7 @@ class CategoryTestCase(TestCase):
         """
 
         category_name_2 = self.category_2.title
-        self.assertEqual(self.category_name_2.__str__(), category_name_2, "USER STRING METHOD NOT WORK")
+        self.assertEqual(self.category_2.__str__(), category_name_2, "USER STRING METHOD NOT WORK")
 
 
 class ProductTestCase(TestCase):
@@ -37,18 +36,16 @@ class ProductTestCase(TestCase):
 
     def setUp(self):
         super(ProductTestCase, self).setUp()
-        # self.product_1 = Product.objects.get(pk=1)
-        # import pdb;pdb.set_trace()
+        self.product_1 = Product.objects.get(pk=1)
         self.product_2 = Product.objects.get(pk=2)
-        self.product_3 = Product.objects.get(pk=3)
 
     def test_pass_stringification(self):
         """
         Test the stringification of a ``Product`` object. A
         "human-readable" representation of an ``Product`` object.
         """
-        product_name_2 = self.product_2.product_name
-        self.assertEqual(self.product_2.__str__(), product_name_2, "USER STRING METHOD WORK")
+        product_name_1 = self.product_1.product_name
+        self.assertEqual(self.product_1.__str__(), product_name_1, "USER STRING METHOD WORK")
 
     def test_fail_stringification(self):
         """
@@ -56,5 +53,5 @@ class ProductTestCase(TestCase):
         "human-readable" representation of an ``Product`` object.
         """
 
-        product_name_3 = self.product_3.product_name
-        self.assertEqual(self.product_3.__str__(), product_name_3, "USER STRING METHOD NOT WORK")        
+        product_name_2 = self.product_2.product_name
+        self.assertEqual(self.product_2.__str__(), product_name_2, "USER STRING METHOD NOT WORK")        
